14ebb996c1bb5ef8f92e477ad8a75e795413e3e3
work <https://github.com/soce768sc900/jpogfdsgo.git>
2018-06-23 15:36:11 +0800

    mvl;fdbml

---
+++ homework-new/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace homework_new
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string A = @"C:\temp\data.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void VVV_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ToDoITem ToDoList = new ToDoITem();
+            ToDo.Children.Add(ToDoList);
+        }
+
+        private void ADDD_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ToDoITem ToDoList = new ToDoITem();
+            ToDo.Children.Add(ToDoList);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                int a = 0;
+
+                foreach (ToDoITem item in ToDo.Children)
+                {
+
+                    a += item.OH;
+                }
+                Money.Text = a.ToString();
+            }
+        }
+        private void DeleteItem(object sender, EventArgs e)
+        {
+            ToDo.Children.Remove((ToDoITem)sender);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            List<string> datas = new List<string>();
+
+            foreach (ToDoITem item in ToDo.Children)
+            {
+                string line = "";
+​                line += "|" + item.time.Text + "|" + item.ITEMnameTb.Text + "|" + item.OH.ToString();
+                datas.Add(line);
+
+            }
+
+            System.IO.File.WriteAllLines(A, datas);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] lines = System.IO.File.ReadAllLines(A);
+​
+   
+    foreach (string line in lines)
+            {
+
+                string[] parts = line.Split('|');
+
+                ToDoITem item = new ToDoITem();
+                item.ITEMname = parts[1];
+                item.DeleteItem += new EventHandler(DeleteItem);
+​
+      
+        ToDo.Children.Add(item);
+            }
+        }
+        public static void Main()
+        {
+            string B = @"C:\temp\datt.txt";
+            
+            }
+
+    }
+    }
+
---
+++ homework-new/ToDoITem.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace homework_new
+{
+    /// <summary>
+    /// ToDoITem.xaml 的互動邏輯
+    /// </summary>
+    public partial class ToDoITem : UserControl
+    {
+        public event EventHandler DeleteItem;
+
+        public ToDoITem()
+        {
+            InitializeComponent();
+        }
+
+        public string Time
+        {
+            get
+            {
+                return time.Text;
+
+            }
+            set
+            {
+                time.Text = value.ToString();
+            }
+        }
+
+
+        public string ITEMname
+        {
+            get
+            {
+                return ITEMnameTb.Text;
+            }
+            set
+            {
+                ITEMnameTb.Text = value.ToString();
+            }
+        }
+        public int OH
+        {
+            get
+            {
+                try
+                {
+                    return int.Parse(TEXXT.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("必須輸入數字!!");
+                    return 0;
+                }
+            }
+            set
+            {
+                TEXXT.Text = value.ToString();
+            }
+        }
+    }
+}
